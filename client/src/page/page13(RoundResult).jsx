ad9defe1ab28c763fc12169122cc23a1cfd4a0ef
wangenius <wangenius@qq.com>
2025-07-27 01:48:40 +0800

    temp

--- client/src/page/page13(RoundResult).jsx
+++ client/src/page/page13(RoundResult).jsx
@@ -7,5 +7,7 @@
       <div className="w-8 h-3 left-[13.40px] top-[5.56px] absolute bg-white" />
     </div>
   </div>
-  <div className="w-80 left-[39px] top-[699px] absolute text-center justify-start text-white text-xl font-normal font-['Cactus_Classical_Serif'] leading-relaxed">第一个月，团队迎来一次曝光机会，在谨慎与激进中取得微妙平衡。你们被市场注意到了一点，但真正的认可仍需产品上线来证明自己。</div>
-</div>
+  <div className="w-80 left-[39px] top-[699px] absolute text-center justify-start text-white text-xl font-normal font-['Cactus_Classical_Serif'] leading-relaxed">
+    第一个月，团队迎来一次曝光机会，在谨慎与激进中取得微妙平衡。你们被市场注意到了一点，但真正的认可仍需产品上线来证明自己。
+  </div>
+</div>;
